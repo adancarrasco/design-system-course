3d9cf67def87b0b5a5a212649d4ea0e8b093708c
Adán Carrasco <adan.carrasco@hotmail.com>
2020-05-10 21:15:59 +0200

    Add Styling for Atom|Button

--- src/atoms/Button/Button.component.tsx
+++ src/atoms/Button/Button.component.tsx
@@ -5,13 +5,17 @@ export interface IButtonProps {
   StartIcon?: React.ReactType
   children?: React.ReactNode | string
   EndIcon?: React.ReactType
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
 
-const Button: React.FC<IButtonProps> = ({ StartIcon, children, EndIcon }) => {
+const resolveChildren = (children) =>
+  typeof children === 'function' ? children : <span>{children}</span>
+
+const Button: React.FC<IButtonProps> = ({ StartIcon, children, EndIcon, onClick = () => {} }) => {
   return (
-    <ButtonStyled>
+    <ButtonStyled onClick={onClick}>
       {StartIcon && <StartIcon />}
-      {children && children}
+      {children && resolveChildren(children)}
       {EndIcon && <EndIcon />}
     </ButtonStyled>
   )
@@ -19,10 +23,36 @@ const Button: React.FC<IButtonProps> = ({ StartIcon, children, EndIcon }) => {
 
 interface ButtonStyledProps {
   color?: string
+  backgroundColor?: string
+  backgroundColorHover?: string
+  fontSize?: string
+  padding?: string
 }
 
 const ButtonStyled = styled.button<ButtonStyledProps>`
-  color: ${(props) => props.color};
+  color: ${(props) => props.color ?? '#fff'};
+  background-color: ${(props) => props.backgroundColor ?? 'rgba(130, 130, 130, 0.5)'};
+  font-size: ${(props) => props.fontSize ?? '1rem'};
+  padding: ${(props) => props.padding ?? '0.5rem'};
+  cursor: pointer;
+  border: none;
+  border-radius: 4px;
+  & svg:first-of-type {
+    margin-right: 4px;
+  }
+  & svg:last-child {
+    margin-left: 4px;
+  }
+  & svg:only-child {
+    margin: 0;
+  }
+  & > svg,
+  & > span {
+    vertical-align: middle;
+  }
+  &:hover {
+    background-color: ${(props) => props.backgroundColorHover ?? 'rgba(145, 145, 145, 0.5)'};
+  }
 `
 
 export default Button
--- src/atoms/Button/Button.stories.tsx
+++ src/atoms/Button/Button.stories.tsx
@@ -6,11 +6,11 @@ import Button from '.'
 
 storiesOf('Atoms|Button', module)
   .add('Default', () => <Button>This is a button</Button>)
-  .add('With StartIcon', () => <Button StartIcon={AccessAlarm}>Access Alarm Icon</Button>)
-  .add('With EndIcon', () => <Button EndIcon={ThreeDRotation}>ThreeDRotation</Button>)
-  .add('With both icons', () => (
+  .add('StartIcon', () => <Button StartIcon={AccessAlarm}>Access Alarm Icon</Button>)
+  .add('EndIcon', () => <Button EndIcon={ThreeDRotation}>ThreeDRotation</Button>)
+  .add('Both icons', () => (
     <Button StartIcon={AccessAlarm} EndIcon={ThreeDRotation}>
       Both Icons
     </Button>
   ))
-  .add('One icon', () => <Button StartIcon={AccessAlarm} />)
+  .add('Icon only', () => <Button StartIcon={AccessAlarm} />)
