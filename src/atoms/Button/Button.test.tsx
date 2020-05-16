bc7f3111952eda0bbefa43f3f28aa4fbfeebf001
Adán Carrasco <adan.carrasco@hotmail.com>
2020-05-16 23:12:10 +0200

    Remove unused icon

--- src/atoms/Button/Button.test.tsx
+++ src/atoms/Button/Button.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import { AccessAlarm, ThreeDRotation } from '@material-ui/icons'
+import { AccessAlarm } from '@material-ui/icons'
 
 import Button, { IButtonProps } from '.'
 
