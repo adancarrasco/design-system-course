6b12f58ea18550f09cde624284015d541d4a00ed
Adán Carrasco <adan.carrasco@hotmail.com>
2020-05-17 14:50:26 +0200

    Fix conflict

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import '@testing-library/jest-dom'
-=======
->>>>>>> master
 import serializer, { matchers } from 'jest-emotion'
 expect.addSnapshotSerializer(serializer)
 expect.extend(matchers)
